1eb98c48521e169855f082cced9496333679bfed
Sundarleo11 <sundarrock1999@gmail.com>
2022-11-12 17:48:44 +0530

    fixedthecookieissue

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ require("dotenv").config();
 require('./config/db').connect();
 const express = require('express');
 const bcrypt = require('bcryptjs');
-const jwt=require('jsonwebtoken');
-const User = require('./model/user');
-const auth=require('./middleware/auth');
+const jwt = require('jsonwebtoken');
 const cookieParser = require("cookie-parser");
+const User = require('./model/user');
+const auth = require('./middleware/auth');
+
 
 const app = express();
 app.use(express.json());
@@ -22,95 +23,95 @@ app.get("/desborad", auth, (req, res) => {
     res.status(200).send("<h1>welcome to Dasborad<h1/>");
 
 })
-    app.post('/register', async (req, res) => {
+app.post('/register', async (req, res) => {
 
     try {
-        const { firstname, lastname, email, password}=req.body;
-        if (!( email && password && firstname && lastname )) {
+        const { firstname, lastname, email, password } = req.body;
+        if (!(email && password && firstname && lastname)) {
             res.status(400).send('All field are required');
         }
-      const sameUser= await User.findOne({ email }); //promise
-    
-      
+        const sameUser = await User.findOne({ email }); //promise
+
+
         // console.log("coment1");
         if (sameUser) {
             //console.log("coment2");
             return res.status(401).send('user already exists');
         }
         //console.log("coment23");
-        const myEncPassword = await bcrypt.hash(password,10);
+        const myEncPassword = await bcrypt.hash(password, 10);
         //const myEncPassword = await bcrypt.hashSync(password, 10);
-       const user=User.create({
+        const user = User.create({
             firstname,
             lastname,
-            email:email.toLowerCase(),
-            password:myEncPassword
+            email: email.toLowerCase(),
+            password: myEncPassword
         });
-        const token =jwt.sign(
-            {user_id: user._id,email},
+        const token = jwt.sign(
+            { user_id: user._id, email },
             process.env.SECRTET_KEY,
             {
-                expiresIn:"2h"
+                expiresIn: "2h"
             }
 
         );
-        user.token =token;
+        user.token = token;
         //update or not in DB
-        
+
         //TODO:handle password situation
-         user.password = undefined;
-         return res.status(201).json(user);
-    
+        user.password = undefined;
+        return res.status(201).json(user);
+
     } catch (error) {
         console.log(error);
     }
 
 
 
-   
+
 });
 
-app.post("/login",async (req,res)=>{
+app.post("/login", async (req, res) => {
 
     try {
-        const{email,password}=req.body;
+        const { email, password } = req.body;
 
-        if(!(email && password)){
+        if (!(email && password)) {
             return res.status(404).send('pls signup');
         }
-         const user= await User.findOne({ email });
-        if(user && (await bcrypt.compare(password, user.password))){
-            const token =jwt.sign(
-                {user_id: user._id,email},
+        const user = await User.findOne({ email });
+        if (user && (await bcrypt.compare(password, user.password))) {
+            const token = jwt.sign(
+                { user_id: user._id, email },
                 process.env.SECRTET_KEY,
                 {
-                    expiresIn:"2h"
+                    expiresIn: "2h"
                 }
-    
-            );
-            user.token =token;
-             //update or not in DB
-        
-        //TODO:handle password situation
-         user.password = undefined;
-         //return res.status(201).json(user);
 
-         //setting up cookies
-         const option ={
-             expires :new Date(Date.now() + 3 * 24 * 60* 60 * 1000),
-             httpOnly:true,
-         };
-         return res.status(200).cookie('tooken',token,option).json({
-             success: true,
-             token,
-             user,
-         });
+            );
+            user.token = token;
+            //update or not in DB
+
+            //TODO:handle password situation
+            user.password = undefined;
+            //return res.status(201).json(user);
+
+            //setting up cookies
+            const option = {
+                expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+                httpOnly: true,
+            };
+            return res.status(200).cookie('token', token, option).json({
+                success: true,
+                token,
+                user,
+            });
         }
         return res.status(401).send("email or password is incorrect");
     } catch (error) {
         console.log(error);
     }
 
-  })
+})
 
 module.exports = app;
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,26 +1,29 @@
-const jwt=require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+const user = require("../model/user");
 
-const auth=(req,res,next)=>{
-console.log(req.cookies);
-const token=  
-req.cookies.token ||
-req.body.token ||
-req.header("Authorization");//.replace("Bearer ","");
+const auth = (req, res, next) => {
+    console.log(req.cookies);
+    //console.log(req.header)
+    const token =
+        req.cookies.token ||
+        req.body.token ||
+        req.header("Authorization").replace("Bearer ", "");
 
 
-if(!token){
-   res.status(403).send("token is missing");
-   
+    if (!token) {
+        returnres.status(403).send("token is missing");
+
+    }
+
+    try {
+        const decode = jwt.verify(token, process.env.SECRTET_KEY);
+        console.log(decode);
+        req.user = decode;
+    } catch (error) {
+        //  console.log(error)
+        return res.status('401').send("Invalid token");
+    }
+    return next();
 }
 
-try {
- const decode= jwt.verify(token, process.env.SECRTET_KEY);
- console.log(decode);
-} catch (error) {
-  //  console.log(error)
-  return res.status('401').send("Invalid token");
-}
- return next();
-}
-
-module.exports=auth;
+module.exports = auth;
